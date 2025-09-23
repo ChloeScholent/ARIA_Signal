a21e6d6b3a96174bb93e55c5ecc46ba2c23752e6
ChloeScholent <159830563+ChloeScholent@users.noreply.github.com>
2025-09-23 20:38:59 +0200

    Update stereo_to_mono.py

--- stereo_to_mono.py
+++ stereo_to_mono.py
@@ -1,12 +1,15 @@
 from pydub import AudioSegment
+import os
 
-stereo_audio = AudioSegment.from_file("Chardonneret_stereo.wav", format="wav")
+stereo_files = ["XC702143.wav", 'XC972996.wav']
 
-mono_audios = stereo_audio.split_to_mono()
+for file in stereo_files:
+    extension = os.path.splitext(file)[1].lstrip(".")
+    filename = os.path.splitext(os.path.basename(file))[0]
+    stereo_audio = AudioSegment.from_file(file, format=extension)
+    mono_audios = stereo_audio.split_to_mono()
+    mono_left = mono_audios[0].export(f'{filename}_left.wav', format='wav')
+    mono_right = mono_audios[1].export(f'{filename}_right.wav', format='wav')
 
-mono_left = mono_audios[0].export(
-    "Chardonneret_left.wav",
-    format="wav")
-mono_right = mono_audios[1].export(
-    "Chardonneret_right.wav",
-    format="wav")
+
+
