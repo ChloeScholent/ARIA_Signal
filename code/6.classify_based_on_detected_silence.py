f4889a3f5ac652ca79db29cec48b72085e8a8fc7
= <=>
2025-11-05 14:47:29 +0100

    minor changes

--- code/6.classify_based_on_detected_silence.py
+++ code/6.classify_based_on_detected_silence.py
@@ -19,7 +19,7 @@ for file in os.listdir(folder):
     y, sr = librosa.load(file_path, sr=None)
 
     # Non-silent intervals
-    intervals = librosa.effects.split(y, top_db=20)
+    intervals = librosa.effects.split(y, top_db=25)
 
     # Derive silent intervals
     silent_intervals = []
@@ -33,7 +33,7 @@ for file in os.listdir(folder):
     silence_samples = []
     for interval in silent_intervals:
         silence_len = abs(interval[1] - interval[0])
-        if silence_len < 5500:
+        if silence_len < 6000:
             silence_samples.append(silence_len)
         
     if len(silence_samples) == 0:
@@ -41,14 +41,14 @@ for file in os.listdir(folder):
 
     minim.append((len(silence_samples), file_path))
  
-    # # Plot
-    # plt.figure(figsize=(12, 4))
-    # librosa.display.waveshow(y, sr=sr, alpha=0.7)
-    # for s, e in silent_intervals:
-    #     plt.axvspan(s / sr, e / sr, color='gray', alpha=0.4)
-    # plt.title(f"Detected Silences in Birdsong ({file})")
-    # plt.xlabel("Time (s)")
-    # plt.show()
+    # Plot
+    plt.figure(figsize=(12, 4))
+    librosa.display.waveshow(y, sr=sr, alpha=0.7)
+    for s, e in silent_intervals:
+        plt.axvspan(s / sr, e / sr, color='gray', alpha=0.4)
+    plt.title(f"Detected Silences in Birdsong ({file})")
+    plt.xlabel("Time (s)")
+    plt.show()
 
 sorted_minim = sorted(minim)
 print("Files with most silent sequences:")
